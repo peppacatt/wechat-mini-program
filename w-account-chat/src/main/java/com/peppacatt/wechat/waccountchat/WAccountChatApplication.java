1f68aabab318db056c9cf7f420df1b1cf5b419b1
peppacatt <peppacatt@163.com>
2023-10-31 23:06:27 +0800

    init

---
+++ w-account-chat/src/main/java/com/peppacatt/wechat/waccountchat/WAccountChatApplication.java
@@ -0,0 +1,13 @@
+package com.peppacatt.wechat.waccountchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WAccountChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WAccountChatApplication.class, args);
+    }
+
+}
