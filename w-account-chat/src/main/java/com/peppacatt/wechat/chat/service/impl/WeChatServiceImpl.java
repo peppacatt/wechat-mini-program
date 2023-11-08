8fee08d9c6cc2169dfb83017ca022c7be1920f4b
peppacatt <peppacatt@163.com>
2023-11-08 23:19:43 +0800

    被动回复消息

---
+++ w-account-chat/src/main/java/com/peppacatt/wechat/chat/service/impl/WeChatServiceImpl.java
@@ -0,0 +1,102 @@
+package com.peppacatt.wechat.chat.service.impl;
+
+import cn.hutool.crypto.SecureUtil;
+import com.peppacatt.wechat.chat.service.WeChatService;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.*;
+
+/**
+ * 微信公众号重要逻辑service实现类
+ */
+@Service
+@Slf4j
+public class WeChatServiceImpl implements WeChatService {
+
+    /**
+     * 验证消息的确来自微信服务器
+     *
+     * @param signature   微信加密签名，signature结合了开发者填写的token参数和请求中的timestamp参数、nonce参数。
+     * @param timestamp   时间戳
+     * @param nonce       随机数
+     * @param echostr     如果验证成功,原样返回该值
+     * @param wechatToken 微信公众号自己设置的Token
+     * @return echostr 入参echostr
+     */
+    public String verify(String signature, String timestamp, String nonce, String echostr, String wechatToken) {
+        // 1）将token、timestamp、nonce三个参数进行字典序排序
+        List<String> mySignatures = Arrays.asList(wechatToken, timestamp, nonce);
+        Collections.sort(mySignatures);
+        // 2）将三个参数字符串拼接成一个字符串进行sha1加密
+        StringBuilder mySignatureStr = new StringBuilder();
+        for (String temp : mySignatures) {
+            mySignatureStr.append(temp);
+        }
+        String sha1Str = SecureUtil.sha1(mySignatureStr.toString());
+        // 3）开发者获得加密后的字符串可与signature对比，标识该请求来源于微信
+        if (signature.equals(sha1Str)) {
+            log.info("验证微信服务器成功！");
+            return echostr;
+        } else {
+            log.error("验证微信服务器失败, sinature: {}, mySignature: {}", signature, sha1Str);
+            return null;
+        }
+    }
+
+    /**
+     * 接收微信服务器的消息
+     *
+     * @param request request
+     * @return 回复的消息
+     */
+    public String receiveMsg(HttpServletRequest request) {
+        // 获取inputStream
+        ServletInputStream inputStream;
+        try {
+            inputStream = request.getInputStream();
+        } catch (IOException e) {
+            log.error("getInputStream fail! ex: {}", e.getMessage());
+            return null;
+        }
+        // 解析xml
+        SAXReader saxReader = new SAXReader();
+        Document document;
+        try {
+            document = saxReader.read(inputStream);
+        } catch (DocumentException e) {
+            log.error("read inputStream fail! ex: {}", e.getMessage());
+            return null;
+        }
+        Element rootElement = document.getRootElement();
+        List<Element> elements = rootElement.elements();
+        Map<String, String> map = new HashMap<>();
+        for (Element element : elements) {
+            map.put(element.getName(), element.getStringValue());
+        }
+        System.out.println(map);
+        return replyMsg();
+    }
+
+    /**
+     * 被动回复用户消息
+     *
+     * @return
+     */
+    private String replyMsg() {
+        return String.format("<xml>\n" +
+                "  <ToUserName><![CDATA[%s]]></ToUserName>\n" +
+                "  <FromUserName><![CDATA[%s]]></FromUserName>\n" +
+                "  <CreateTime>%d</CreateTime>\n" +
+                "  <MsgType><![CDATA[%s]]></MsgType>\n" +
+                "  <Content><![CDATA[%s]]></Content>\n" +
+                "</xml>", "oRvWB6R8twtTH3ON1Z40bs8wBDtw", "gh_28f994e163a8", new Date().getTime(), "text", "我回复了一条消息");
+    }
+}
